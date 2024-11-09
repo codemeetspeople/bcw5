43b6f85edaa3a8f9ee4986c5a3ca73b220d16c82
Oleksandr Kunzietsov <caiman@7sky.dev>
2024-11-09 16:08:06 +0200

    Data structures

---
+++ data.py
@@ -0,0 +1,17 @@
+# array = (1, 2, 3, 4, 5, 6)
+# print(array)
+# print(array.count(4))
+# print(array.index(4))
+# print(len(array))
+
+# array = [1, 2, 3, 4, 5]
+# array.append(10)
+# array.insert(0, 5)
+# array.extend([1, 2, 3, 4, 5])
+# print(array)
+
+array = (1, 2, 3)
+array = list(array)
+array[0] = 42
+array = tuple(array)
+print(array)
